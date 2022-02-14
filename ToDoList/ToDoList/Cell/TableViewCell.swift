536d11ac374fd1fb8380ad29083dfa373349b0d2
svysotsky <svysotsky@intervale.ru>
2022-02-14 14:27:53 +0300

    Add Xib TableViewCell Add UITableView in InBoxViewController

---
+++ ToDoList/ToDoList/Cell/TableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  TableViewCell.swift
+//  ToDoList
+//
+//  Created by Sergey Vysotsky on 14.02.2022.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
--- ToDoList/ToDoList/ViewControllers/InBoxViewController.swift
+++ ToDoList/ToDoList/ViewControllers/InBoxViewController.swift
@@ -9,12 +9,68 @@ import UIKit
 
 class InBoxViewController: UIViewController {
 
+    var tableView = UITableView()
+    let identifier = "IDCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.prefersLargeTitles = true
         view.backgroundColor = .white
         self.title = "In Box"
+        
+        createTableView()
     }
     
+    func createTableView(){
+        tableView = UITableView(frame: view.bounds, style: .insetGrouped)
+        
+        let nib = UINib(nibName: "TableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: identifier)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        tableView.sectionFooterHeight = 0
+        
+        view.addSubview(tableView)
+    }
+}
+
+extension InBoxViewController: UITableViewDelegate, UITableViewDataSource{
+    //MARK: - Section
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
     
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 40))
+        let lbl = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 40))
+        lbl.text = "Section \(section)"
+        view.addSubview(lbl)
+        
+        return view
+    }
+
+    //MARK: - Cell
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        40
+    }
+        
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as? TableViewCell
+        
+        cell?.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+        cell?.nameLabel.text = "adasdas"
+        return cell ?? TableViewCell()
+    }
 }

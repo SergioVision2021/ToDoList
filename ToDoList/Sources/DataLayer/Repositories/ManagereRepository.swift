0d8704b62f5584fe2459d18570a97c3234c14a4b
Sergey Vysotsky <svysotsky@vysotsky-mac.intervale.ru>
2022-10-31 17:18:20 +0300

    [task_1.5] ref: change script select repository in ManagerRepository

--- ToDoList/Sources/DataLayer/Repositories/ManagereRepository.swift
+++ ToDoList/Sources/DataLayer/Repositories/ManagereRepository.swift
@@ -7,53 +7,58 @@
 
 import Foundation
 
-struct ManagerRepository: Repository {
+enum TypeRepository {
+    case remote
+    case cache
+}
 
+class ManagerRepository: Repository {
+
+    private var currentType = TypeRepository.remote
+    private var currentRepository: Repository
+    private var remoteDataSource: RemoteDataSource
+    private var casheDataSource: CasheDataSource
+    
     init(remoteDataSource: RemoteDataSource, casheDataSource: CasheDataSource) {
         self.remoteDataSource = remoteDataSource
         self.casheDataSource = casheDataSource
+        self.currentRepository = remoteDataSource
     }
 
     func fetch(_ completionHandler: @escaping FetchCompletionHandler) {
 
-        remoteDataSource.fetch { (result) in
-        //casheDataSource.fetch { (result) in
+        currentRepository.fetch { (result) in
             switch result {
             case .success(let dataModel):
                 completionHandler(Result.success(dataModel))
             case .failure(let error):
-                completionHandler(Result.failure(error))
+                
+                self.currentType = .cache
+                self.currentRepository = self.casheDataSource
+                
+                self.currentRepository.fetch { (result) in
+                    switch result {
+                    case .success(let dataModel):
+                        completionHandler(Result.success(dataModel))
+                    case .failure(let error):
+                        completionHandler(Result.failure(error))
+                    }
+                }
             }
         }
     }
 
-//        remoteDataSource.fetch { (result) in
-//            switch result {
-//            case .success(let dataModel):
-//                self.casheDataSource.save(dataModel) { (result) in
-//                    switch result {
-//                    case.success(_): break
-//                    case.failure(_): break
-//                    }
-//
-//                }
-//                completionHandler(result)
-//            case .failure(_):
-//                self.casheDataSource.fetch(completionHandler)
-//            }
-//        }
-
     func update(_ operation: Operations, _ task: Task, _ data: [Group], completionHandler: @escaping (Error?) -> ()) {
  
-//        casheDataSource.save(data) {
-//            completionHandler($0)
-//        }
-
-        remoteDataSource.update(operation: operation, task) {
-            completionHandler($0)
+        switch currentType {
+        case .remote:
+            remoteDataSource.update(operation: operation, task) {
+                completionHandler($0)
+            }
+        case .cache:
+            casheDataSource.save(data) {
+                completionHandler($0)
+            }
         }
     }
-
-    private var remoteDataSource: RemoteDataSource
-    private var casheDataSource: CasheDataSource
 }
